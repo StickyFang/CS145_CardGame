1ae6b720585d34cbf22db92d6e4efa7efdce7548
Bryce Nyswonger <131291492+StickyFang@users.noreply.github.com>
2023-06-01 14:53:29 -0700

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,40 @@
+public class Card {
+    private final String suit; // Hearts, Clubs, Spades, Diamonds
+    private final int rank; // 1-13
+
+    public Card(String suit, int rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+
+    public String toString() {
+        String rankString;
+        switch (rank) {
+            case 1:
+                rankString = "Ace";
+                break;
+            case 11:
+                rankString = "Jack";
+                break;
+            case 12:
+                rankString = "Queen";
+                break;
+            case 13:
+                rankString = "King";
+                break;
+            default:
+                rankString = String.valueOf(rank);
+                break;
+        }
+        return rankString + " of " + suit;
+    }
+}
